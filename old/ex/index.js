2abc11232c8d235985fd8a928624340d3b97f62f
Everson <eversonpereiraofc@gmail.com>
2022-03-24 14:01:20 -0300

    configurando loader para css

--- old/ex/index.js
+++ old/ex/index.js
@@ -1,14 +1,9 @@
-const produto = {
-    nome: "Caneta",
-    preco: 1.90,
-    desconto: 0.05
-}
+import "./estilo.css";
+import "react";
 
-function clone(objeto){
-    return {...objeto}
-}
 
-const novoProduto = clone(produto)
-novoProduto.nome = "Caneta Preta"
+export default props => (
+    <h1>Olá</h1>
+)
 
-console.log(produto, novoProduto)
+console.log("Funcionou!")
